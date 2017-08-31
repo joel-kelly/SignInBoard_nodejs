4327aadd044af9029e54772c7d5cb8ca77a0d1bc
joel-kelly <joel.alexander.kelly@gmail.com>
2017-08-31 16:42:51 -0700

    iOS style in/out toggles
    -Pure CSS toggles in the style of iOS
    -Socket is working
    - Cleaned up test and dummy files

--- index.js
+++ index.js
@@ -4,42 +4,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var GoogleSpreadsheet = require('google-spreadsheet');
 
-// Google Spreadsheet Credentials
-var creds = require('./client_secret.json');
- 
-// Create a document object using the ID of the spreadsheet - obtained from its URL.
-var doc = new GoogleSpreadsheet('1XhFDcj4bgYa2-XTHXaedQVnEye-oROtaa4DfynJdCsY');
- 
-// Google Spreadsheet table is organized as follows:
-//name,	inout,	contactphone,	contactemail,	emergencyphone,	emergencyname,	emergencyrelation
-//NB Google's row API strips header cells to remove whitespace characters and symbols
-
-var people = [];
-
-
-
- 
-function generateList(doc) { 
-// Authenticate with the Google Spreadsheets API.
-doc.useServiceAccountAuth(creds, function (err) {
-	var foo = [];
-  // Get all of the rows from the spreadsheet.
-  doc.getRows(1, function (err, rows) {
-
-	rows.forEach(function (entry) {
-		foo.unshift(entry);
-		console.log('List size is', foo.length);
-	});
-	  return foo;
-  });
-});
-}
-//GS code executes last?
-
-
-console.log(generateList(doc));
-//console.log('Outside function list size is', people.length);
-
+//Start NodeJS 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -57,3 +22,27 @@ http.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+//Set up Google Spreadsheet table
+// Google Spreadsheet Credentials
+var creds = require('./client_secret.json');
+ 
+// Create a document object using the ID of the spreadsheet - obtained from its URL.
+var doc = new GoogleSpreadsheet('1XhFDcj4bgYa2-XTHXaedQVnEye-oROtaa4DfynJdCsY');
+
+
+//Create a socket connection- we will query the db and emit it every time a client connects
+io.on('connection', function (socket) {
+
+    console.log('a client connected');
+
+// Authenticate with the Google Spreadsheets API.
+doc.useServiceAccountAuth(creds, function (err) {
+ 
+  // Get all of the rows from the spreadsheet.
+  doc.getRows(1, function (err, rows) {
+      //console.log('Data received from Db:\n');
+      //console.log(rows);
+	  socket.emit('showrows',rows);
+	});
+  });
+});	
